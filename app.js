76d5ebcbd7534c27956db64a5dc14ea8437ac431
Sonu-Shushant-14 <158523202+Sonu-Shushant-14@users.noreply.github.com>
2024-06-13 18:32:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const hrEle = document.querySelector(".hr");
+const minEle = document.querySelector(".min");
+const secEle = document.querySelector(".sec  ");
+
+
+setInterval(() => {
+    let date = new Date();
+
+    hr = date.getHours()*30
+
+    min = date.getMinutes()*6
+
+    sec = date.getSeconds()*6
+
+     hrEle.style.transform = `rotateZ(${(hr) + min/12}deg)`
+
+     minEle.style.transform = `rotateZ(${(min) + min/12}deg)`
+
+
+     secEle.style.transform = `rotateZ(${(sec) + min/12}deg)`
+
+})
